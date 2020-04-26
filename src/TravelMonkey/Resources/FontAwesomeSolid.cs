d1d612b3e7161e74d1cf8c1283932bc552695761
Bohdan Benetskyi (PGS Software) <bbenetskyi@pgs-soft.com>
2020-04-26 14:34:12 +0200

    Eject Icon Names into static C# file

---
+++ src/TravelMonkey/Resources/FontAwesomeSolid.cs
@@ -0,0 +1,9 @@
+namespace TravelMonkey.Resources
+{
+    public static class FontAwesomeSolid
+    {
+        public const string Info = "\uf05a";
+        public const string Globe = "\uf7a2";
+        public const string Language = "\uf1ab";
+    }
+}
